8298808f3249a868324266eea3abfd9d5754d07e
Conner Hasbrouck <connerhasbrouck@gmail.com>
2018-11-20 13:13:56 -0600

    Create button.js

---
+++ button.js
@@ -0,0 +1,20 @@
+var rpio = require('rpio');
+
+var buttonPin = 7;
+var ledPin = 11;
+
+rpio.open(buttonPin, rpio.INPUT, rpio.PULL_DOWN);
+
+function pollcb(cbpin) {
+  var state = rpio.read(cbpin) ? 1 : 0;
+
+  if (state == 1) {
+    console.log('button pressed');
+    rpio.write(ledPin, rpio.HIGH);
+  } else {
+    console.log('button released');
+    rpio.write(ledPin, rpio.LOW);
+  }
+}
+
+rpio.poll(buttonPin, pollcb);
